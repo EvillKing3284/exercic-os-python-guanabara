81d5f36a8f23053695c4635bff6710157f6a4017
EvillKing3284 <158593915+EvillKing3284@users.noreply.github.com>
2025-12-16 14:12:41 -0300

    atualizando - ex064-certo.py

---
+++ ex064.py
@@ -0,0 +1,13 @@
+usuario = int(input("Digite um número [999 para parar]: "))
+soma = 0
+contador = 0
+while usuario != 999:
+    contador = contador + 1
+    soma = soma + usuario
+    usuario = int(input("Digite um número [999 para parar]: "))
+
+
+else:
+    print(f"Voçê digitou {contador} números e a soma entre eles foi {soma} ")
+
+
