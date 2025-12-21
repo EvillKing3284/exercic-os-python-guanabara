8c077f5e9c329e5c17a407071ff52ef529c53e01
EvillKing3284 <158593915+EvillKing3284@users.noreply.github.com>
2025-12-20 23:16:23 -0300

    corrigindo

---
+++ ex068b.py
@@ -0,0 +1,31 @@
+from random import randint
+
+linha = "=-=-=-=-=-=-=-" * 2
+print(f"{linha}\nVAMOS JOGAR PAR OU ÍMPAR\n{linha}\n")
+
+contador = 0
+
+while True:
+    computador = randint(1, 10)
+    usuario = int(input("Diga um valor: "))
+    opcao = input("Par ou Ímpar [P/I]: ").upper()
+
+    soma = usuario + computador
+    resultado_par = soma % 2 == 0
+
+    print(f"Você jogou {usuario} e o computador {computador}. Total de {soma}", end=" ")
+
+    if resultado_par:
+        print("deu PAR")
+    else:                                                                   #Condições simplificadas da IA
+        print("deu ÍMPAR")
+
+    # vitória
+    if (opcao == "P" and resultado_par) or (opcao == "I" and not resultado_par):
+        print("Você venceu!\n")
+        contador += 1
+    else:
+        print("Você perdeu!")
+        break
+
+print(f"\nVocê acertou {contador} vezes")
--- ex069.py
+++ ex069.py
@@ -1,35 +1,4 @@
-from random import randint
-print(f"{"=-=-=-=-=-=-=-"*2}\nVAMOS JOGAR PAR OU ÍMPAR\n{"=-=-=-=-=-=-=-"*2}\n")
-contador = 0
-while True:
 
-    computador = randint(1, 10)
-    usuario = int(input("Diga um valor: "))
-    opcao = str(input("Par ou Ímpar [P/I]: ").upper())
-    soma = usuario + computador
-    if soma % 2 == 0:
-        print(f"Voçê jogou {usuario} e computador {computador}. Total de {soma} deu par")
-    else:
-        print(f"Voçê jogou {usuario} e computador {computador}. Total de {soma} deu ímpar")
-    if opcao == "I":
-        if soma % 2 != 0:
-            resultado = "Voçê Venceu!"
-            contador += 1
-        if soma % 2 == 0:
-            resultado = "Voçê perdeu"
-            print(resultado)
-            break
-    elif opcao == "P":
-        if soma % 2 == 0:
-            resultado = "Voçê Venceu!"
-            contador += 1
-        if soma % 2 != 0:
-            resultado = "Voçê perdeu!"
-            print(resultado)
-            break
-    print(resultado)
-
-print(f"Voçê acertou {contador} vezes")
 
 
 
