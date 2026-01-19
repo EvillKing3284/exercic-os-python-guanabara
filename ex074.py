97c8774201d62e84954900c6acd26c03c2dcc268
EvillKing3284 <158593915+EvillKing3284@users.noreply.github.com>
2026-01-19 09:50:30 -0300

    exercício 074 concluído

--- ex074.py
+++ ex074.py
@@ -1,5 +1,16 @@
-soma = 0
-for c in range(0, 11):
-    print(c)
-    soma = soma + c
-print(soma)
+from random import randint
+maior_numero = 0
+menor_numero = None
+tupla = (randint(1,10),randint(1,10),randint(1,10),randint(1,10))
+for c in tupla:
+    if c > maior_numero :
+        maior_numero = c
+    if menor_numero is None or c < menor_numero:
+        menor_numero = c
+
+print(f"Os valores sorteados foram: {" ".join(map(str,tupla))}") #revisar esta técnica com map
+#Esse primeiro print poderia ser feito de outra forma mais fácil, tipo assim:
+#for c in tupla:
+    #print(c,end=" ")
+print(f"O maior valor sorteado foi: {maior_numero}")
+print(f"O menor valor sorteado foi: {menor_numero}")
